6bac24bc46c4d3bfe52c1e0af76d32b8e89fe0ce
Davidruiz03061976 <davidruizdemanuel39@gmail.com>
2025-05-16 06:54:59 +0000

    Cambios en el archivo app.py para probar el metodo GET

--- aventura-social/backend/src/app.py
+++ aventura-social/backend/src/app.py
@@ -1,19 +1,27 @@
 from flask import Flask
 from flask_cors import CORS
-from src.models import db
 from flask_migrate import Migrate
+from src.models import db
 from src.routes.users import users_bp
 from src.routes.events import events_bp
 
-# Inicializa la app y permite peticiones desde otros origenes
+# Inicializa la app y permite peticiones desde otros orígenes
 app = Flask(__name__)
 CORS(app)
 
+# Configuración base de datos
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///SPORTCONNECT-G1.db'
-app.config['SQLALCHEMY_TRANK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Inicialización de extensiones
 db.init_app(app)
 migrate = Migrate(app, db)
 
-
+# Registro de rutas
 app.register_blueprint(users_bp, url_prefix='/api/users')
 app.register_blueprint(events_bp, url_prefix='/api/events')
+
+# Ruta base para testeo rápido
+@app.route('/')
+def index():
+    return {"message": "API Aventura Social funcionando"}
