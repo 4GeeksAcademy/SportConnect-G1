18aa1310d81ecd40ff59406ede94c4c54f0b5357
GuidoCanso <guidocansobre@gmail.com>
2025-05-16 14:00:39 +0000

    Home y Login terminados

--- aventura-social/frontend/src/App.jsx
+++ aventura-social/frontend/src/App.jsx
@@ -1,16 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 // import './App.css'
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
+import CreateEvent from './pages/CreateEvent';
+import Home from './pages/Home';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/create-event" element={<CreateEvent />} />
       </Routes>
     </Router>
   );
