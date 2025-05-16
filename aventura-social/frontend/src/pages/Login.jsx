cddd517abe8d3755cb67ab7601ec65d8010acf51
GuidoCanso <guidocansobre@gmail.com>
2025-05-16 09:09:31 +0000

    Login terminado

--- aventura-social/frontend/src/pages/Login.jsx
+++ aventura-social/frontend/src/pages/Login.jsx
@@ -15,17 +15,16 @@ const Login = () => {
             <div className="col-md-6 order-1 order-md-2 d-flex flex-column justify-content-center align-items-center text-white">
                 <div className="w-75">
                     <div className="d-flex align-items-center justify-content-center mb-4 gap-2">
-                        <img src="/logo.png" alt="Logo" style={{ width: "40px" }} />
-                        <h2 className="m-0 text-success">SportConnect</h2>
+                        <img src="/logo_sin_fondo.png" alt="Logo" style={{ width: "400px" }} />
                     </div>
                     <h4 className="mb-3 text-black">Log in</h4>
                     <form>
                         <div className="mb-3">
-                            <label htmlFor="email" className="form-label">Email</label>
+                            <label htmlFor="email" className="form-label text-black">Email</label>
                             <input type="email" className="form-control" id="email" />
                         </div>
                         <div className="mb-4">
-                            <label htmlFor="password" className="form-label">Password</label>
+                            <label htmlFor="password" className="form-label text-black">Password</label>
                             <input type="password" className="form-control" id="password" />
                         </div>
                         <button type="submit" className="btn btn-light w-100 text-white bg-success">Log in</button>
