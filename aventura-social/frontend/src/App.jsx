aa7cf6ad0ddc07d1ac0f122d7f656690a358b140
GuidoCanso <guidocansobre@gmail.com>
2025-05-17 15:28:50 +0000

    Profile terminado

--- aventura-social/frontend/src/App.jsx
+++ aventura-social/frontend/src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import CreateEvent from './pages/CreateEvent';
 import Home from './pages/Home';
+import Profile from './pages/Profile';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/create-event" element={<CreateEvent />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </Router>
   );
